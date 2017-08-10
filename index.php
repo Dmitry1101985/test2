a6d697eecfa5d696825d19adafca3d37b96fe03b
Dmitry1101985 <dmitry.sherstyuk85@gmail.com>
2017-08-10 18:38:25 +0300

    add class Article

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-phpinfo();
+class Article{
+    public $title;
+    public $text;
+    public $author;
+
+    public function view(){
+        echo $this->title . "<br/>";
+        echo $this->text . "<br/>";
+        echo $this->author . "<br/>";
+    }
+}
+
+$art = new Article();
+$art->title = "Title";
+$art->text = "This is some large text? you know...";
+$art->author = "Me";
+
+$art->view();
